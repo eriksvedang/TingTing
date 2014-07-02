453fcc2b18727d1ba565de7245478217a8778a25
Erik <erik.svedang@gmail.com>
2014-07-02 10:45:02 +0200

    Trying to optimize Room

--- TingTing/src/Pathfinding/Room.cs
+++ TingTing/src/Pathfinding/Room.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Linq;
 using Pathfinding;
 using System.Diagnostics;
+using System.Text;
 
 namespace TingTing
 {
@@ -29,16 +30,33 @@ namespace TingTing
         private ValueEntry<bool> CELL_exterior = null;
         private ValueEntry<IntPoint> CELL_worldPosition = null;
 
+        // Optimization
+        private ValueEntry<string> CELL_optiGrid; // a string of 0:s and 1:s that makes up a grid of where the IntPoints should be created
+        private ValueEntry<IntPoint> CELL_optiGridSize; // the width and height of the grid
+        private ValueEntry<IntPoint> CELL_optiGridOffset; // where the top left corner of the grid starts
+
         protected override void SetupCells()
         {
             CELL_name = EnsureCell<string>("name", "undefined");
-            CELL_tiles = EnsureCell<IntPoint[]>("tiles", new IntPoint[] { });
-            IntPoint[] points = CELL_tiles.data;
-            if (points.Length > 0) {
-                SetTiles(points);
-            }
             CELL_exterior = EnsureCell<bool>("exterior", false);
             CELL_worldPosition = EnsureCell<IntPoint>("worldPosition", IntPoint.Zero);
+
+            CELL_optiGrid = EnsureCell<string>("optiGrid", "");
+            CELL_optiGridSize = EnsureCell<IntPoint>("optiGridSize", new IntPoint(0, 0));
+            CELL_optiGridOffset = EnsureCell<IntPoint>("optiGridOffset", new IntPoint(0, 0));
+
+            CELL_tiles = EnsureCell<IntPoint[]>("tiles", new IntPoint[] { });
+
+            if (optiGrid == "") {
+                // no opti grid for this room yet
+                IntPoint[] points = CELL_tiles.data;
+                if (points.Length > 0) {
+                    SetTiles(points);
+                }
+            } else {
+                //Console.WriteLine("Room " + name + " has an optimized grid. Will load it now!");
+                LoadTilesFromOptigrid();
+            }
         }
      
         #region ACCESSORS
@@ -48,6 +66,7 @@ namespace TingTing
             foreach (PointTileNode ptn in _tilesByLocalPositionHash.Values) {
                 AddTileLinks(ptn);
             }
+                
             RefreshTileData();
         }
 
@@ -122,6 +141,33 @@ namespace TingTing
             }
         }
 
+        public string optiGrid {
+            get {
+                return CELL_optiGrid.data;
+            }
+            set {
+                CELL_optiGrid.data = value;
+            }
+        }
+
+        public IntPoint optiGridSize {
+            get {
+                return CELL_optiGridSize.data;
+            }
+            set {
+                CELL_optiGridSize.data = value;
+            }
+        }
+
+        public IntPoint optiGridOffset {
+            get {
+                return CELL_optiGridOffset.data;
+            }
+            set {
+                CELL_optiGridOffset.data = value;
+            }
+        }
+
         #endregion
 
         public void SetTiles(IList<IntPoint> pPoints)
@@ -139,6 +185,117 @@ namespace TingTing
             UpdateBounds();
         }
 
+        public void SetOptigrid(IList<IntPoint> pPoints) {
+            Console.WriteLine("Will set optigrid for room " + name);
+
+            if (pPoints.Count == 0) {
+                optiGrid = "";
+                optiGridSize = new IntPoint(0, 0);
+                optiGridOffset = new IntPoint(0, 0);
+                Console.WriteLine("No tiles in room " + name + ", will return");
+                return;
+            }
+
+            int lowestX = int.MaxValue;
+            int lowestY = int.MaxValue;
+            int highestX = int.MinValue;
+            int highestY = int.MinValue;
+
+            foreach (var p in pPoints) {
+                if (p.x < lowestX) {
+                    lowestX = p.x;
+                }
+                if (p.y < lowestY) {
+                    lowestY = p.y;
+                }
+                if (p.x > highestX) {
+                    highestX = p.x;
+                }
+                if (p.y > highestY) {
+                    highestY = p.y;
+                }
+            }
+
+            IntPoint upperLeft = new IntPoint(lowestX, lowestY);
+            IntPoint lowerRight = new IntPoint(highestX, highestY);
+
+            //Console.WriteLine(name + " upper left: " + upperLeft + ", lower right: " + lowerRight);
+
+            optiGridOffset = upperLeft;
+            optiGridSize = (lowerRight - upperLeft) + new IntPoint(1, 1);
+            int totalPointsInGrid = optiGridSize.x * optiGridSize.y;
+            //Console.WriteLine("Size: " + optiGridSize + " total points in grid: " + totalPointsInGrid);
+
+            char[] positions = new char[totalPointsInGrid];
+
+            for (int i = 0; i < positions.Length; i++) {
+                positions[i] = '0';
+            }
+
+            int gridWidth = optiGridSize.x;              
+            foreach (var p in pPoints) {
+                int normalizedX = p.x - optiGridOffset.x;
+                int normalizedY = p.y - optiGridOffset.y;
+                int index = normalizedY * gridWidth + normalizedX;
+                //Console.WriteLine("Placing tile at index " + index + " (based on point " + p + ")");
+                positions[index] = '1';
+            }
+
+            optiGrid = new string(positions);
+            //Console.WriteLine("Final opti grid: " + optiGrid);
+
+            // Remove any tiles that are saved the old (SLOW) way
+            CELL_tiles.data = new IntPoint[] { };
+
+            LoadTilesFromOptigrid();
+        }
+
+        void LoadTilesFromOptigrid()
+        {
+            _tilesByLocalPositionHash.Clear();
+
+            string gridString = optiGrid;
+            int gridWidth = optiGridSize.x;
+            int gridHeight = optiGridSize.y;
+
+            /*
+            int offsetX = optiGridOffset.x;
+            int offsetY = optiGridOffset.y;
+
+            int index = 0;
+            foreach (char c in gridString) {
+                if (c == '1') {
+                    int x = (index % gridWidth);
+                    int y = (index / gridWidth);
+                    int xx = x + offsetX;
+                    int yy = y + offsetY;
+                    PointTileNode newNode = new PointTileNode(new IntPoint(xx, yy), this);
+                    _tilesByLocalPositionHash[newNode.localPoint.GetHashCode()] = newNode;
+                }
+                index++;
+            }
+            if (index != gridWidth * gridHeight) {
+                throw new Exception("Index doesn't match size of grid");
+            }*/
+
+            for (int y = 0; y < gridHeight; y++) {
+                for (int x = 0; x < gridWidth; x++) {
+                    int index = y * gridWidth + x;
+                    int xx = x + optiGridOffset.x;
+                    int yy = y + optiGridOffset.y;
+                    if (gridString[index] == '1') {
+                        //Console.WriteLine("Will create (optimized) tile at room pos " + xx + ", " + yy + " !!!");
+                        PointTileNode newNode = new PointTileNode(new IntPoint(xx, yy), this);
+                        _tilesByLocalPositionHash[newNode.localPoint.GetHashCode()] = newNode;
+                    } else {
+                        //Console.WriteLine("No tile at room pos " + xx + ", " + yy);
+                    }
+                }
+            }
+           
+            UpdateBounds();
+        }
+
         /// <summary>
         /// Warning, too slow to run for each tile during load
         /// </summary>
@@ -147,6 +304,10 @@ namespace TingTing
             #if DEBUG && LOG
             Console.WriteLine("Called Room.ApplyTileData() WARNING, this is SUPER SLOW. Callstack: " + Environment.StackTrace);
             #endif
+
+            if (optiGrid != "") {
+                return;
+            }
 
             CELL_tiles.data = (from PointTileNode n in _tilesByLocalPositionHash.Values select n.localPoint).ToArray();
         }
@@ -174,6 +335,11 @@ namespace TingTing
 
         private void RefreshTileData()
         {
+            if (optiGrid != "") {
+                // Should not have to refresh tile data if there is an optimized grid in this room
+                return;
+            }
+
             CELL_tiles.data = (from PointTileNode n in _tilesByLocalPositionHash.Values select n.localPoint).ToArray();
         }
 
@@ -259,7 +425,13 @@ namespace TingTing
         }
 
         public IntPoint[] points {
-            get { return CELL_tiles.data; }
+            get { 
+                if (optiGrid == "") {
+                    return CELL_tiles.data;
+                } else {
+                    return _tilesByLocalPositionHash.Values.Select(t => t.localPoint).ToArray();
+                }
+            }
         }
 
         private void ConnectNodes(PointTileNode pA, PointTileNode pB)
